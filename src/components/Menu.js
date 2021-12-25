a8ab89197b2c97c886c3da01d7094caacfe5615b
Sparrow48 <abunasib48@gmail.com>
2021-12-26 00:01:04 +0600

    add modal

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { Icon } from "@iconify/react";
 
 function Menu() {
@@ -6,20 +7,38 @@ function Menu() {
 
   const toggle = () => {
     setShowMenu(!showMenu);
-    console.log(`Clicked showMenu toggle button, showMenu: ${showMenu}`);
+    console.log(`Clicked showMenu toggle button, showMenu: ${!showMenu}`);
   };
+
   return (
     <nav className="lg:hidden">
-      <div className="flex flex-row justify-between px-5">
-        <h1>e-Shop</h1>
-        <Icon
-          icon="bx:bx-menu-alt-right"
-          color="blue"
-          width="48"
-          height="48"
-          onClick={toggle}
-        />
-      </div>
+      {showMenu ? (
+        <div className="flex flex-row">
+          <div className="bg-gray-50 h-screen w-4/5 z-50">
+            <h1>e-Shop</h1>
+            <ul className="flex flex-col">
+              <NavLink to="/home">Home</NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/product">Product</NavLink>
+            </ul>
+          </div>
+          <div
+            className="items-end bg-gray-700 w-1/5 h-screen"
+            onClick={toggle}
+          ></div>
+        </div>
+      ) : (
+        <div className="flex flex-row justify-between px-5">
+          <h1>e-Shop</h1>
+          <Icon
+            icon="bx:bx-menu-alt-right"
+            color="blue"
+            width="48"
+            height="48"
+            onClick={toggle}
+          />
+        </div>
+      )}
     </nav>
   );
 }
