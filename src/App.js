0613610d58421d3f87398a9bf18e586fd64a770d
Sparrow48 <abunasib48@gmail.com>
2021-12-25 19:36:17 +0600

    setup for react and tailwind css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" content-center  text-5xl text-gray-600 font-medium italic">
+        Hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
